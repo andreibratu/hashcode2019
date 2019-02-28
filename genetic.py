9e88cf4ccb48deaf6afd2b0e09cfd87f931fa642
Andrei Bratu <bratu.andrei@outlook.co>
2019-02-28 22:15:57 +0200

    fixed individual generation

--- genetic.py
+++ genetic.py
@@ -4,7 +4,7 @@ from util import read_input, write_output
 # Hyperparameters
 GENERATIONS = 1000
 TUPLE_SIZE_BOUND = 5
-SWAP_SLIDES_CHANCE_BOUND = 0.2
+SWAP_SLIDES_MUTATION_CHANCE_BOUND = 0.2
 DISCARD_PHOTO_CHANCE = 0.1
 SERIES = 50
 SLIDES_FROM_SERIE = 10000
@@ -16,8 +16,19 @@ series = generate_photo_series([p.id for p in photos], SERIES, DISCARD_PHOTO_CHA
 
 for s in series:
     for s in range(SLIDES_FROM_SERIE):
-        s =
-        individual = generate_individual(s)
-        print(individual)
-        individual = [Slide(p1, p2) for p1, p2 in individual]
-        POOL.append(individual)
+        for i in range(SLIDES_FROM_SERIE):
+            individual = generate_individual(s)
+            print(individual)
+            # Build the slide back from ids
+            slides = []
+            for idx1, idx2 in individual:
+                p1 = photos[idx1]
+                if p2 is not None:
+                    p2 = photos[idx2]
+                s = Slide(p1, p2)
+                slides.append(s)
+
+            individual = Individual(slide)
+            POOL.append(individual)
+
+print(POOL)
