c2c647eb133e381d44a8bb27cd7e75fe029641bb
Andrei Bratu <bratu.andrei@outlook.co>
2019-03-04 13:37:17 +0200

    Amended discard strategies to account for even vertical photos amount

--- strategy/discard_strategy.py
+++ strategy/discard_strategy.py
@@ -3,6 +3,21 @@ from typing import List, Callable
 from models.photo import Photo
 
 
+def _add_vertical_photo(old_set: List[Photo], new_set: List[Photo]):
+    """Auxiliary function that mentains the even vertical photos restriction.
+
+    Function appends to `new_set` a vertical photo that is not in it.
+    """
+
+    vertical_photos_remain = [p for p in new_set if p.orientation == 'V']
+    # Check if vertical photos are in even number
+    if len(vertical_photos_remain) % 2 == 0:
+        # Odd number of vertical photos, add one more
+        vertical_photo = [p for p in old_set \
+                          if p.orientation == 'V' and p not in new_set][0]
+        old_set.append(vertical_photo)
+
+
 def keep_all_photos(photos: List[Photo]):
     """Keep the initial set."""
 
@@ -18,7 +33,10 @@ def discard_outliers(margin: float) -> Callable:
         l_bound = avg - (avg * float)
         u_bound = avg + (avg * float)
 
-        return [p for p in photos if l_bound <= len(p.tags) <= u_bound]
+        remain = [p for p in photos if l_bound <= len(p.tags) <= u_bound]
+        _add_vertical_photo(photos, remain)
+
+        return remain
 
     assert 0 < margin < 1
     return f
@@ -29,7 +47,9 @@ def discard_random(percent: float) -> Callable:
 
     def f(photos: List[Photo]) -> List[Photo]:
         how_many_remain = len(photo) - int(len(photo) * percent)
-        return random.sample(photos, how_many_remain)
+
+        remain = random.sample(photos, how_many_remain)
+        _add_vertical_photo(photos, remain)
 
     assert 0 < margin < 1
     return f
